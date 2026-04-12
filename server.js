ac34a02077732d74e5efc2bc1a13976b72d493cc
Praneeth R P <rpraneeth108@gmail.com>
2026-04-12 23:00:39 +0530

    cors fixed

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ const app = express();
 
 app.set("trust proxy", 1);
 
-app.use(cors({
-  origin: "*",
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
