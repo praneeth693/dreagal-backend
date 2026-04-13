af40c07c899522e6f597e97f698cd43400da6e9a
Praneeth R P <rpraneeth108@gmail.com>
2026-04-13 17:10:02 +0530

    fixed smtp mail

--- server.js
+++ server.js
@@ -33,12 +33,14 @@ const razorpay = new Razorpay({
 const sendMail = async (order) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
